e10805984ff032560e94c8018e19f73e1c2ef36a
Melvin Suter <44713851+melvin-suter@users.noreply.github.com>
2024-07-25 09:53:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 mkdir -p  /opt/bato-scripts
 
-curl "https://api.github.com/repos/melvin-suter/scripts.bash/contents" | grep -i '"name"' | awk -F':' '{print $2}' | sed -E 's;[", ];;g' | grep -E '^bato-.*$' | while read file ; do
-    curl https://raw.githubusercontent.com/melvin-suter/scripts.bash/master/$file > /opt/bato-scripts/$file
+curl "https://api.github.com/repos/melvin-suter/bato/contents" | grep -i '"name"' | awk -F':' '{print $2}' | sed -E 's;[", ];;g' | grep -E '^bato-.*$' | while read file ; do
+    curl https://raw.githubusercontent.com/melvin-suter/bato/master/$file > /opt/bato-scripts/$file
     chmod 555 /opt/bato-scripts/$file
     chown root:root /opt/bato-scripts/$file
 done
